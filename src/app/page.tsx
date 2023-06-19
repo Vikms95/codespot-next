b0056e99497f0e74adfeb1b2e65f4718fbdbf6c2
Vikms95 <vms1995@hotmail.com>
2023-06-20 01:01:48 +0200

    style: :lipstick: add outline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,51 @@
 'use client';
-import '../assets/global.css';
-import { useState } from 'react';
-import { NavbarWithGuest, NavbarWithUser } from '../components/Navbar';
-import { AppLayout } from '../layouts';
+import { useEffect } from 'react';
+import { TSetter } from '@/types';
+import { LazyPostPreview as PostPreview } from '@/components/PostPreview';
+import { PostsLayout } from '@/layouts';
+import { usePostsContext } from '@/context/PostsContext';
+import { getPosts } from '@/services/post';
+import { StyledHome } from '@/components/Home/_styles';
+import useSWR from 'swr';
 
-export default function Home() {
-	return;
+type Props = {
+	setLastClickedPostId: TSetter<string>;
+	setIsModalActive: TSetter<boolean>;
+};
+
+export default function Home({
+	setLastClickedPostId,
+	setIsModalActive,
+}: Props) {
+	const { posts, setPosts } = usePostsContext();
+	const { data } = useSWR('/api/posts', getPosts);
+
+	useEffect(() => {
+		setPosts(data?.reverse());
+	}, [data]);
+
+	return (
+		<StyledHome>
+			{posts && (
+				<PostsLayout title='Latest post' section='home'>
+					{posts.map(
+						post =>
+							post.public && (
+								<PostPreview
+									key={post._id}
+									id={post._id}
+									user={post.user}
+									text={post.text}
+									title={post.title}
+									image={post.image}
+									timestamp={post.timestamp}
+									setIsModalActive={setIsModalActive}
+									setLastClickedPostId={setLastClickedPostId}
+								/>
+							)
+					)}
+				</PostsLayout>
+			)}
+		</StyledHome>
+	);
 }
