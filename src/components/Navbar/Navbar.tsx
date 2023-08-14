7c0f71416b976d169784b34f278c4877edcdff55
Vikms95 <vms1995@hotmail.com>
2023-08-14 22:25:18 +0200

    feat: :sparkles: do the thing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,43 @@
 'use client';
 import { useAuth } from '@hooks/useAuth';
-import { useWindowDimensions } from '@hooks/useWindowDimensions';
-import { FaHouseUser } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
 
-import {
-	InnerNav,
-	NavArrow,
-	NavItem,
-	StyledLink,
-	StyledNavbar,
-	TitleText,
-} from './_styles';
 import Link from 'next/link';
 import { NavbarText } from './NavbarText';
+import { MobileNavbar } from './MovileNavbar';
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
 
 type Props = { children: JSX.Element[] };
 
 export function Navbar({ children }: Props) {
 	useAuth();
+	const [mobileNavbar, setMobileNavbar] = useState(false);
 
 	return (
 		<nav data-testid='navigation-bar' className='bg-white flex'>
-			<ul className=' group list-none flex flex-row justify-start  w-full gap-x-7 mb-5 '>
+			<ul className='sm:flex hidden group list-none flex-row justify-start w-full gap-x-7 mb-5'>
 				<Link className='navbar-link' data-testid='home-button' href='/'>
 					<NavbarText text='Home' />
 				</Link>
 				{children}
 			</ul>
+
+			<FaBars
+				onClick={() => setMobileNavbar(true)}
+				className='sm:hidden flex hover:cursor-pointer'
+			/>
+
+			{mobileNavbar && <MobileNavbar />}
 		</nav>
 	);
 }
