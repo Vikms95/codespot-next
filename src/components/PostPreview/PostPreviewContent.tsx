cf839a336b9703cc0e935b98c8bd8fcb0e7c431d
Vikms95 <vms1995@hotmail.com>
2023-09-11 20:02:04 +0200

    style: style

--- src/components/PostPreview/PostPreviewContent.tsx
+++ src/components/PostPreview/PostPreviewContent.tsx
@@ -21,16 +21,16 @@ export function PostPreviewContent({
 	return (
 		<>
 			<article className='flex justify-between overflow-hidden'>
-				<h3 className='m-0 mb-[0.8rem] text-sm text-[#8d8d8d] flex self-end '>
+				<h3 className='m-0 mb-[0.8rem] flex self-end text-sm text-[#8d8d8d] '>
 					{username}
 				</h3>
-				<h3 className='m-0 mb-[0.8rem] text-sm text-[#8d8d8d] flex self-end '>
+				<h3 className='m-0 mb-[0.8rem] flex self-end text-sm text-[#8d8d8d] '>
 					{timestamp}
 				</h3>
 			</article>
 
 			<h2 className='m-0 text-lg'>{title}</h2>
-			<div ref={textRef} className='line-clamp-3 p-0 mb-4' />
+			<div ref={textRef} className='mb-4 line-clamp-3 p-0' />
 		</>
 	);
 }
